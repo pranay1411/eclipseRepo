8f2854385c41af6700aed2f12af9831b074f2562
pranay1411 <36498706+pranay1411@users.noreply.github.com>
2019-01-28 16:24:26 +0530

    Update TestingClass.java
    beautified

--- GitTesting/src/TestingClass.java
+++ GitTesting/src/TestingClass.java
@@ -1,11 +1,8 @@
 
 public class TestingClass {
 
-	
 	int a = 0;
 	
 	public static void main(String[] args) {
-		// varibale changess
 		System.out.println("sefesfe regrgr rd gg");
-		//sysout	
 	}
