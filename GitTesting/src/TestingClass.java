3d4ee3d2352021e3a37771dc5a8dd6ae9eb5faba
chinta.pranay <pranaytheglazingfire@gmail.com>
2019-01-28 16:26:18 +0530

    sysout

--- GitTesting/src/TestingClass.java
+++ GitTesting/src/TestingClass.java
@@ -1,11 +1,7 @@
 
 public class TestingClass {
 
-	
-	int a = 0;
-	
+	static int a = 0;
 	public static void main(String[] args) {
-		// varibale changess
-		System.out.println("sefesfe regrgr rd gg");
-		//sysout	
+		System.out.println("a " a);
 	}
