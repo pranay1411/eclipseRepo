2820d91225126fcba6d54d66492d7e0d7b4abec8
pranay1411 <36498706+pranay1411@users.noreply.github.com>
2019-01-31 12:48:51 +0530

    Update TestingClass.java
    only m1 method

--- GitTesting/src/TestingClass.java
+++ GitTesting/src/TestingClass.java
@@ -5,11 +5,7 @@ public class TestingClass {
 	public static void main(String[] args) {
 		System.out.println("a "+ a);
 	}
-	static void m2(){
-		System.out.println("a "+ a);
-	}
-	
 	static void m1(){
-		//TODO
+		System.out.println("a "+ a);
 	}
 }
